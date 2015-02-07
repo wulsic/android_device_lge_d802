e4a7bc162671feaa0d9c963a7b6dd8f10f31a532
Wulsic <wulsic@hotmail.com>
2015-02-07 17:23:20 +0100

    Omnify

---
+++ omni.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2013 OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/d802/full_d802.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := d802
+PRODUCT_NAME := omni_d802
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-D802
+PRODUCT_MANUFACTURER := lge
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_open_com/g2:4.4.2/KOT49I/D80220d.1397037197:user/release-keys PRIVATE_BUILD_DESC="g2_open_com-user 4.4.2 KOT49I D80220d.1397037197 release-keys"
+
+PRODUCT_PACKAGES += Torch
